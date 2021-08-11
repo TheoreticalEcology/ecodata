7beaeb07d2feb107424bff11ff6b28efd3f96452
EttnerAndreas <andi.ettner@yahoo.de>
2021-08-11 12:04:03 +0200

    running devtools::check(cleanup = TRUE), to delete output log directory

--- EcoData.Rcheck/00_pkg_src/EcoData/R/EcoData.R
+++
@@ -1,9 +0,0 @@
-#' @title EcoData - A collection of ecological datasets for teaching
-#' @name EcoData
-#' @author Florian Hartig 
-#' @docType package
-#' @description A collection of ecological datasets for teaching
-#' @details See index / vignette for details
-#' @examples 
-#' vignette("EcoData", package="EcoData")
-NULL
