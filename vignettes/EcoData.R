369f7a062f331cf1e54118f2dcba8c66221558d2
Florian Hartig <florianhartig@users.noreply.github.com>
2022-06-02 14:40:04 +0200

    moved package to root

---
+++ vignettes/EcoData.R
@@ -0,0 +1,6 @@
+## ---- echo = F----------------------------------------------------------------
+library(EcoData)
+
+## ----global_options, include=FALSE--------------------------------------------
+knitr::opts_chunk$set(fig.width=7, fig.height=4.5, fig.align='center', warning=FALSE, message=FALSE, cache = F)
+
