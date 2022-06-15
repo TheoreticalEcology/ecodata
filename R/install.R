90cfbe45ae7105948e5a060d10b44e477f4af223
Florian Hartig <florianhartig@users.noreply.github.com>
2022-06-15 14:55:26 +0200

    updates

---
+++ R/install.R
@@ -0,0 +1,23 @@
+getPackages <- function(){
+  
+  
+  packages = "
+  DHARMa,
+  glmmTMB,
+  lme4,
+  spatial,
+  sf,
+  gstat,
+  effects,
+  mapview,
+  missRanger,
+  ranger,
+  dplyr,
+  stringr,
+  forcats,
+  keras,
+  tensorflow,
+  qpdf
+  "
+  
+}
